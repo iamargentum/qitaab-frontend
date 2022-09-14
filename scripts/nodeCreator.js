0f9e72ffe6b940dbbd06bc21d3f4649b6bfa14b6
iamargentum <rajat.rj97@gmail.com>
2022-09-14 22:31:57 +0530

    Purge project

--- scripts/nodeCreator.js
+++
@@ -1,7 +0,0 @@
-function nodeSelected(event) {
-    console.log("type of selected node is ", event.target.value);
-}
-
-let selectElement = document.getElementById("nodeTypeSelector")
-
-selectElement.onchange = nodeSelected
