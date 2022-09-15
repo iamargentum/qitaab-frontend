6e076a793df04fcdda3c714341c8f046fae41ee5
iamargentum <rajat.rj97@gmail.com>
2022-09-15 18:52:36 +0530

    Recreate project with Svelte + Vite

---
+++ src/lib/Counter.svelte
@@ -0,0 +1,11 @@
+<script>
+  let count = 0
+  const increment = () => {
+    count += 1
+  }
+  $: doubled = count*2
+</script>
+
+<button on:click={increment}>
+  count is {count} and doubled is {doubled}
+</button>
