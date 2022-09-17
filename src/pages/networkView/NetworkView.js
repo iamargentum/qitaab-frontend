207bb7c851274f94ce860c3235e42525fc2636a2
iamargentum <rajat.rj97@gmail.com>
2022-09-18 01:03:41 +0530

    Add essential components to the network view

--- src/pages/networkView/NetworkView.js
+++ src/pages/networkView/NetworkView.js
@@ -1,5 +1,26 @@
+import { useState } from "react";
+import { NetworkGraph } from "./NetworkGraph";
+import { SideBar } from "./SideBar";
+
 export function NetworkView() {
+
+    let [nodeData, setNodeData] = useState(undefined)
+    let [sideBarOpen, setSideBarOpen] = useState(false)
+
+    let nodeClicked = (data) => {
+        setNodeData(nodeData)
+        setSideBarOpen(true)
+    }
+
+    let sideBarShowHideTest = () => {
+        setSideBarOpen(prev => !prev)
+    }
+
     return (
-        <h1>this is the network view</h1>
+        <div className="w-full h-screen bg-black p-3 overflow-hidden">
+            <button className="float-left bg-white" onClick={sideBarShowHideTest}>show/hide sidebar</button>
+            <NetworkGraph nodeClicked={nodeClicked} />
+            <SideBar open={sideBarOpen} nodeData={nodeData} />
+        </div>
     )
 }
