f5f212e193e3a7a07a7d25a3e4853f90fc8f163b
iamargentum <rajat.rj97@gmail.com>
2022-09-18 11:57:45 +0530

    Update sidebar style (very bad, needs to be changed)

--- src/pages/networkView/SideBar.js
+++ src/pages/networkView/SideBar.js
@@ -1,8 +1,6 @@
-import { createRef, useEffect } from "react"
-
 export function SideBar({open, nodeData}) {
 
-    const defaultStyle = "bg-white float-right rounded-md h-full w-96"
+    const defaultStyle = "bg-white float-right rounded-md h-full w-96 fixed top-0 right-0 absolute top-0 right-0 z-999"
 
     return (
         <div
